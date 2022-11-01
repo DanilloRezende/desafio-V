a3cd81066f38ffcee87b3767147497b1679c4af4
DanilloRezende <danillo.rezende3@gmail.com>
2022-10-31 23:00:45 -0300

    Concluido Desafio

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -28,7 +28,7 @@ export function Content({ selectedGenre, movies }: ContentProps) {
 
       <main>
         <div className="movies-list">
-          {movies.map(movie => (
+          {movies.map((movie) => (
             <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
           ))}
         </div>
